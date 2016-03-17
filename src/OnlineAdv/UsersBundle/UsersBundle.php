b779df1c39c2a5c8ced810a6448ba87b349b23b6
Virgil Mihai <virgil.mihai@live.com>
2016-03-17 07:23:07 +0200

    sof update

--- src/OnlineAdv/UsersBundle/UsersBundle.php
+++ src/OnlineAdv/UsersBundle/UsersBundle.php
@@ -6,4 +6,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UsersBundle extends Bundle
 {
+    /**
+     * inherits FOS User Bundle
+     * @return FOS User Bundle
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
